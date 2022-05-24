a3c01d3a8bc0e6847167412a18b667d2ae9edcde
twerlp <coocoocast@gmail.com>
2022-05-24 18:01:35 -0400

    Add Cards
    In progress, following https://www.youtube.com/watch?v=C5bnWShD6ng&ab_channel=Blackthornprod

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardManager : MonoBehaviour
+{
+    public List<Card> deck = new List<Card>();
+    public List<Card> discardPile = new List<Card>();
+
+    public Transform[] cardSlots;
+    public bool[] availableCardSlots;
+
+    public TMP_Text deckSizeText;
+    public TMP_Text discardSizeText;
+
+    //Draw a card
+    public void DrawCard() {
+        if (deck.Count >= 1) {
+            Card randCard = deck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++) {
+                if (availableCardSlots[i] == true) {
+                    randCard.gameObject.SetActive(true);
+                    randCard.handIndex = i;
+
+                    randCard.transform.position = cardSlots[i].position;
+                    availableCardSlots[i] = false;
+                    deck.Remove(randCard);
+                }
+            }
+        }
+        deckSizeText.text = deck.Count.ToString(); //Update deck size text
+    }
+    private void Start()
+    {
+        deckSizeText.text = deck.Count.ToString(); //Update deck size text
+        discardSizeText.text = discardPile.Count.ToString(); //Update discard pile size text
+    }
+}
